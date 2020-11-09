8dbe4e0cfc51d2d7ee267fcd3d5a9d03b1447c47
Dual Miguel <dual.giupponigmail.com>
2020-11-09 06:40:46 -0300

    Update ListadoAtracciones and App files

--- TurismoEdadMedia/src/main/App.java
+++ TurismoEdadMedia/src/main/App.java
@@ -1,7 +1,44 @@
 package main;
 
+import java.util.ArrayList;
+
+import TurismoPrincipal.Atraccion;
+import TurismoPrincipal.Enums.ETiposActividades;
+import storage.ListadoAtracciones;
+
 public class App {
-	public void main() {
-		System.out.println("HelloWorld");
+	public static void main(String[] args) {
+		// Creacion de atracciones.
+
+		Atraccion Moria = new Atraccion("Moria", 10, 2.0, 6, ETiposActividades.Aventura);
+		Atraccion MinasTirith = new Atraccion("Minas Tirith", 5, 2.5, 25, ETiposActividades.Paisaje);
+		Atraccion LaComarca = new Atraccion("La Comarca", 3, 6.5, 150, ETiposActividades.Degustacion);
+		Atraccion Mordor = new Atraccion("Mordor", 25, 3.0, 4, ETiposActividades.Aventura);
+		Atraccion AbismoDeHelm = new Atraccion("Abismo de Helm", 5, 2.0, 15, ETiposActividades.Paisaje);
+		Atraccion Lothlorien = new Atraccion("Lothlórien", 35, 1.0, 30, ETiposActividades.Degustacion);
+		Atraccion Erebor = new Atraccion("Erebor", 12, 3.0, 32, ETiposActividades.Paisaje);
+		Atraccion BosqueNegro = new Atraccion("Bosque Negro", 3, 4.0, 12, ETiposActividades.Aventura);
+
+		// Creacion de listado de Aventuras
+		ListadoAtracciones listaAtracciones = new ListadoAtracciones();
+
+		// Carga de las atracciones
+		listaAtracciones.agregarAtraccion(Moria);
+		listaAtracciones.agregarAtraccion(MinasTirith);
+		listaAtracciones.agregarAtraccion(LaComarca);
+		listaAtracciones.agregarAtraccion(Mordor);
+		listaAtracciones.agregarAtraccion(AbismoDeHelm);
+		listaAtracciones.agregarAtraccion(Lothlorien);
+		listaAtracciones.agregarAtraccion(Erebor);
+		listaAtracciones.agregarAtraccion(BosqueNegro);
+
+		// Prueba de los algoritmos
+		ArrayList<Atraccion> listaPrueba = listaAtracciones.getLista(ETiposActividades.Aventura);
+
+		listaPrueba = listaAtracciones.getListaOrdenada();
+
+		listaPrueba = listaAtracciones.getListaOrdenada(ETiposActividades.Aventura);
+
+		System.out.println("Fin");
 	}
 }
--- TurismoEdadMedia/src/storage/ListadoAtracciones.java
+++ TurismoEdadMedia/src/storage/ListadoAtracciones.java
@@ -8,6 +8,9 @@ import TurismoPrincipal.Enums.ETiposActividades;
 public class ListadoAtracciones {
 	ArrayList<Atraccion> lista = new ArrayList<Atraccion>();
 
+	public ListadoAtracciones() {
+	}
+
 	public ListadoAtracciones(ArrayList<Atraccion> atracciones) {
 		this.lista = atracciones;
 	}
@@ -17,23 +20,23 @@ public class ListadoAtracciones {
 		ArrayList<Atraccion> listaAtracciones = new ArrayList<Atraccion>();
 		for (Atraccion atraccion : this.lista) {
 			if (atraccion.esDisponible())
-				lista.add(atraccion);
+				listaAtracciones.add(atraccion);
 		}
 		return listaAtracciones;
 	}
 
-	public ArrayList<Atraccion> getLista(ETiposActividades tipoActividad){
+	public ArrayList<Atraccion> getLista(ETiposActividades tipoActividad) {
 		ArrayList<Atraccion> listaAtracciones = this.getLista();
 		ArrayList<Atraccion> listaFiltrada = new ArrayList<Atraccion>();
-		
-		for(Atraccion atraccion: listaAtracciones) {
-			if(atraccion.tipo() == tipoActividad)
+
+		for (Atraccion atraccion : listaAtracciones) {
+			if (atraccion.tipo() == tipoActividad)
 				listaFiltrada.add(atraccion);
 		}
-		
+
 		return listaFiltrada;
 	}
-	
+
 	public ArrayList<Atraccion> getListaOrdenada() {
 		ArrayList<Atraccion> listaAtracciones = this.getLista();
 
@@ -42,31 +45,43 @@ public class ListadoAtracciones {
 
 	public ArrayList<Atraccion> getListaOrdenada(ETiposActividades tipoActividad) {
 		ArrayList<Atraccion> listaAtracciones = this.getLista(tipoActividad);
-		
+
 		return algoritmoOrdenDineroTiempo(listaAtracciones);
 	}
 
 	public void agregarAtraccion(Atraccion atraccion) {
 		this.lista.add(atraccion);
 	}
-	
-	//Métodos privados de la clase
-	private ArrayList<Atraccion> algoritmoOrdenDineroTiempo(ArrayList<Atraccion> listaAtracciones){
+
+	public void eliminarAtraccion(Atraccion atraccion) {
+		int indice = this.lista.indexOf(atraccion);
+		if (indice != -1)
+			this.lista.remove(indice);
+	}
+
+	// Métodos privados de la clase
+	private ArrayList<Atraccion> algoritmoOrdenDineroTiempo(ArrayList<Atraccion> listaAtracciones) {
 		Atraccion atraccionAux;
 		Atraccion atraccionA;
 		Atraccion atraccionB;
 
 		for (int i = 0; i < listaAtracciones.size(); i++) {
-			for (int j = i; j < listaAtracciones.size(); j++) {
+			for (int j = i + 1; j < listaAtracciones.size(); j++) {
 				atraccionA = listaAtracciones.get(i);
 				atraccionB = listaAtracciones.get(j);
 
 				// Cambia A por B en el array
-				if (atraccionA.costo() > atraccionB.costo() && atraccionA.tiempo() > atraccionB.tiempo()) {
+				if (atraccionA.costo() < atraccionB.costo()) {
+					atraccionAux = atraccionA;
+					listaAtracciones.set(i, atraccionB);
+					listaAtracciones.set(j, atraccionAux);
+				} else if (atraccionA.costo() == atraccionB.costo() && atraccionA.tiempo() < atraccionB.tiempo()) {
 					atraccionAux = atraccionA;
 					listaAtracciones.set(i, atraccionB);
 					listaAtracciones.set(j, atraccionAux);
 				}
+				;
+
 			}
 		}
 
